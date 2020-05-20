59cb1c08eed81db937c9f42af891375f5a4b0e4c
mbalanya <okobacharles@yahoo.co.uk>
2020-05-20 22:22:28 +0300

    created if statements for movie, time and age discount calculator

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
     // UI
 
-    $('form').submit(function(e){     
+    $('form').submit(function(e){
         e.preventDefault();
         // alert("button clicked")
         var movie = $('#movie').val();
         var time = $('#time').val();
         var age = parseInt($('#age').val());
-        alert(typeof time);
+        var movieAttribute = document.getElementById('movie').value;
+        alert(typeof movieAttribute);
+
     });
 });
 // business logic
@@ -17,32 +19,45 @@ function Ticket(movie, time, age){
     this.personAge = age;
 }
 
-Ticket.prototype.determinePrice = function(){
-    var price = 0
-    if (this.personAge > 40 ){
-        price = 20;
-    }else{
+price = 600;
 
-    }
-    return price;
+var ageLogic = function() {
+  if (age < 10) {
+    console.log("person not allowed into theatre");
+  } elseif (age >= 10 && age <= 23) {
+    return price - (price * 0.15);
+  } elseif (age > 50) {
+    return price - (price * 0.1)
+  } else {
+    return price
+  };
 };
 
-var firstTicket = new Ticket();
+var timeLogic = function() {
+  if (time === '9am-12Noon') {
+    return price - (price * 0.15)
+  } elseif (time === '1pm-3pm'){
+    return price - (price * 0.1);
+  } elseif (time === '3pm-6pm') {
+    return price - (price * 0.05)
+  } else {
+    return price
+  };
+};
 
-// business logic
-function Contact(first, last) {
-    this.firstName = first;
-    this.lastName = last;
+
+
+
+var movieLogic = function() {
+  if (movieAttribute === 'repeat') {
+    return price - (price * 0.05);
+  } else {
+    return price;
   }
-  
-  Contact.prototype.fullName = function() {
-    return this.firstName + " " + this.lastName;
-  }
-
-
+};
 
 // price = 600
 // if age < 10 print("person not allowed into theatre"), age >= 10 && age =< 23, discount = 15%,
 //  if age > 50, discount = 10%, else price = 600
-// if time = 9am-12Noon, discount 15%, time = 1pm-3pm, discount= 10%, time = 3pm-6pm, discount = 5%, else price = 600 
+// if time = 9am-12Noon, discount 15%, time = 1pm-3pm, discount= 10%, time = 3pm-6pm, discount = 5%, else price = 600
 // if movie is non first-release, discount 5%
